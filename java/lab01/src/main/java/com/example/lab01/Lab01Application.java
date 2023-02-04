0e467de989043980a97d3b6047a9de78edeaa2ca
manuelzegarra <mzegarra@csonic.cloud>
2023-02-04 13:38:48 -0500

    fast commit

---
+++ java/lab01/src/main/java/com/example/lab01/Lab01Application.java
@@ -0,0 +1,13 @@
+package com.example.lab01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab01Application.class, args);
+    }
+
+}
