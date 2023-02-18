1b927e70635ccec5b0b352fc1f351bd2c18c66aa
manuelzegarra <mzegarra@csonic.cloud>
2023-02-18 10:07:00 -0500

    fast commit

--- java/lab02-order/order-producer/src/main/java/cloud/csonic/orderproducer/service/OrderService.java
+++ java/lab02-order/order-producer/src/main/java/cloud/csonic/orderproducer/service/OrderService.java
@@ -1,9 +1,13 @@
 package cloud.csonic.orderproducer.service;
 
 import cloud.csonic.orderlibrary.event.OrderEvent;
+import org.springframework.kafka.support.SendResult;
+import org.springframework.util.concurrent.ListenableFuture;
 
 public interface OrderService {
     void publish(OrderEvent orderEvent);
     void publishV2(OrderEvent orderEvent);
     void publishV3(OrderEvent orderEvent);
+
+    ListenableFuture<SendResult<Integer, OrderEvent>> publishV4(OrderEvent orderEvent);
 }
